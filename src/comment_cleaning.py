aef77252b8d3bde5b55c2f6036cf4d8456d6b2d5
Brian Posey <brianposey@Brians-MacBook-Pro.local>
2019-09-16 14:37:49 -0700

    Using the proper method to write to csv

--- src/comment_cleaning.py
+++ src/comment_cleaning.py
@@ -22,7 +22,6 @@ if __name__ == "__main__":
 
     small_comments = comments.select('by', 'text', 'time')
 
-    with open("hdfs://ip-10-0-0-15.us-west-2.compute.internal:9000/user/text.txt", "w") as f:
-        f.write(str(small_comments.count()))
+    small_comments.write.csv("test.csv")
 
     spark.stop()
