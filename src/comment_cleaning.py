fed564984934cea8063aa3e11ae9210618b1414f
Brian Posey <brianposey@Brians-MacBook-Pro.local>
2019-09-16 14:51:08 -0700

    And now we're tring multiple approaches

--- src/comment_cleaning.py
+++ src/comment_cleaning.py
@@ -22,6 +22,10 @@ if __name__ == "__main__":
 
     small_comments = comments.select('by', 'text', 'time')
 
+    small_comments.show()
+
+    small_comments.write.csv('mycsv.csv')
+
     small_comments.write.csv('mycsv.csv').save("hdfs://ip-10-0-0-15.us-west-2.compute.internal:9000/user/mycsv.csv")
 
     spark.stop()
