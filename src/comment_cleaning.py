d890917a077cfc634ddaa48739f5868f42684deb
Brian Posey <brianposey@Brians-MacBook-Pro.local>
2019-09-16 14:32:25 -0700

    Writing the output to the HDFS

--- src/comment_cleaning.py
+++ src/comment_cleaning.py
@@ -22,7 +22,7 @@ if __name__ == "__main__":
 
     small_comments = comments.select('by', 'text', 'time')
 
-    with open("text.txt", "w") as f:
+    with open("hdfs://ip-10-0-0-15.us-west-2.compute.internal:9000/user/text.txt", "w") as f:
         f.write(str(small_comments.count()))
 
     spark.stop()
