6df78c91541598f12aa8c3f6d6c782abf14150e2
Brian Posey <brianposey@Brians-MacBook-Pro.local>
2019-09-16 14:07:31 -0700

    figured out that I was saving into the wrong directory

--- src/comment_cleaning.py
+++ src/comment_cleaning.py
@@ -1,10 +1,24 @@
+from __future__ import print_function
+
+import sys
+from operator import add
+from pyspark.sql import SparkSession
 
 path  = "hdfs://ip-10-0-0-15.us-west-2.compute.internal:9000/user/HNI_2018-05.json"
 
-file = spark.read.json(path)
 
-comments = file.filter(file.type=='comment')
+if __name__ == "__main__":
 
-small_comments = comments.select('by', 'text', 'time')
+    spark = SparkSession\
+        .builder\
+        .appName("PythonPi")\
+        .getOrCreate()
+	file = spark.read.json(path)
 
-print(en(small_comments.count()))
+	comments = file.filter(file.type=='comment')
+
+	small_comments = comments.select('by', 'text', 'time')
+
+	print(en(small_comments.count()))
+
+	spark.stop()
