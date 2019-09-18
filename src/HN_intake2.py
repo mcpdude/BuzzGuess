34ef26edba882b1784d49c77bc0b3e417cb31b5a
Brian Posey <brianposey@Brians-MacBook-Pro.local>
2019-09-18 11:39:26 -0700

    didn't add the context right`

--- src/HN_intake2.py
+++ src/HN_intake2.py
@@ -22,7 +22,7 @@ if __name__ == '__main__':
 	conf.setAppName('CopyChat')
 	conf.set('spark.master', 'spark://localhost:7077')
 
-	spark = SparkContext
+	spark = SparkSession.builder.appName('CopyChat').getOrCreate()
 
 	file = spark.read.json(path)
 	comments = file.filter(file.type=='comment')
