60556375d3a8afee5125c7232f278ab7d5cd3030
Brian Posey <brianposey@Brians-MacBook-Pro.local>
2019-09-20 15:49:11 -0700

    oh my god, it compiled. i've fought with importing the ml library for 4 hours

--- build.sbt
+++ build.sbt
@@ -7,6 +7,7 @@ scalaVersion := "2.12.8"
 // https://mvnrepository.com/artifact/org.apache.spark/spark-mllib
 libraryDependencies ++= Seq(
 	"org.apache.spark" %% "spark-core" % "2.4.0",
-	"org.apache.spark" %% "spark-sql" % "2.4.0",
-	"org.apache.spark" %% "spark-mllib" % "2.4.0" % "runtime"
+	"org.apache.spark" %% "spark-sql" % "2.4.0"
 )
+
+libraryDependencies += "org.apache.spark" %% "spark-mllib" % "2.4.0"
--- src/main/scala/HN_intake.scala
+++ src/main/scala/HN_intake.scala
@@ -2,8 +2,10 @@ import org.apache.spark.SparkContext._
 import org.apache.spark.SparkConf
 import org.apache.spark.sql.SparkSession
 import org.apache.spark.sql.functions._
-// import org.apache.spark.ml.feature.{HashingTF, IDF, Tokenizer}
+import org.apache.spark.ml.feature.{HashingTF, IDF, Tokenizer, MinHashLSH}
+
 // import org.apache.spark.ml.linalg.Vectors
+
 
 
 
@@ -12,6 +14,10 @@ object HN_intake {
   def main(args: Array[String]) {
   	val url = "jdbc:postgresql://10.0.0.25:5432/postgres"
   	val path = "hdfs://ip-10-0-0-15.us-west-2.compute.internal:9000/hn/"
+
+  	// val conf = new SparkConf().setAppName("Simple Application")
+  	// val sc = new SparkContext(conf)
+
   	val regex = "(?<!\\w\\.\\w.)(?<![A-Z][a-z]\\.)(?<=\\.|\\?)\\s"
   	val spark = SparkSession.builder().appName("Copy Chat").getOrCreate()
   	import spark.implicits._
@@ -24,16 +30,17 @@ object HN_intake {
     val nice1 = nice.withColumn("clean_sentences", regexp_replace(nice("clean_sentences"), "\\x00", "0"))
     val nice2 = nice1.withColumn("clean_sentences", regexp_replace(nice1("clean_sentences"), "\\\\0x00", "0"))
     // val nice3 = nice2.withColumn("clean_sentences", regexp_replace(nice2("clean_sentences"), "0x00", "0"))
-    nice3.printSchema()
+    // nice3.printSchema()
     val nice5 = nice2.drop("text").drop("sentence")
     nice5.printSchema()
     println("it's working!")
 
-    // val mh = new MinHashLSH().setNumHashTables(5).setInputCol("clean_sentences").setOututCol("sent _hash")
 
-    // val with_hash = mh.fit(nice5)
+    val mh = new MinHashLSH().setNumHashTables(5).setInputCol("clean_sentences").setOutputCol("sent _hash")
 
-    // with_hash.transform(nice5).show()
+    val with_hash = mh.fit(nice5)
+
+    with_hash.transform(nice5).show()
 
     // sentences.write.format("jdbc").option("url", url).option("dbtable", "sentences").option("user", "postgres").mode("overwrite").option("driver", "org.postgresql.Driver").option("password", "L0ngfins").save()
     nice5.write.format("jdbc").option("url", url).option("dbtable", "nice").option("user", "postgres").mode("overwrite").option("driver", "org.postgresql.Driver").option("password", "L0ngfins").save()
