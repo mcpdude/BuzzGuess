7378f2b0c8b79fcfbda5ddf08245e90ea5053bd6
Brian Posey <brianposey@Brians-MacBook-Pro.local>
2019-09-23 14:01:28 -0700

    let's try this again!

--- src/main/scala/HN_intake.scala
+++ src/main/scala/HN_intake.scala
@@ -13,29 +13,40 @@ import org.apache.spark.ml.feature.{CountVectorizer, CountVectorizerModel}
 object HN_intake {
 
   def main(args: Array[String]) {
+
+  	// Paths for the intake data and database
   	val url = "jdbc:postgresql://10.0.0.25:5432/postgres"
   	val path = "s3a://test-insight-data-pipes/hacker_news/"
 
+
+  	// Initializing the tokenizer to split sentences into words later
   	val tokenizer = new Tokenizer().setInputCol("clean_sentences").setOutputCol("words")
 
-
-
-  	// val conf = new SparkConf().setAppName("Simple Application")
-  	// val sc = new SparkContext(conf)
-
+  	// The regex for cleaning comments into sentences
   	val regex = "(?<!\\w\\.\\w.)(?<![A-Z][a-z]\\.)(?<=\\.|\\?)\\s"
+
+  	// Start of the program
   	val spark = SparkSession.builder().appName("Copy Chat").getOrCreate()
   	import spark.implicits._
 
+  	// Read in the data
     val HNdata = spark.read.option("inferSchema", true).json(path)
+    // Filter to only look at comments
     val comments = HNdata.select("by", "time", "text", "id").filter($"type".like("comment"))
+    // Split comments into sentences
     val sentences = comments.select($"by", $"time", $"id", explode(split($"text", regex)).as("sentence"))
-    sentences.printSchema()
+    //sentences.printSchema()
+
+    // Clean the sentences. There were several attempts at this, hence 'nice5'
     val nice = sentences.withColumn("clean_sentences", regexp_replace(sentences("sentence"), "\u0000", "0"))
     val nice1 = nice.withColumn("clean_sentences", regexp_replace(nice("clean_sentences"), "\\x00", "0"))
     val nice5 = nice1.drop("text").drop("sentence")
+
+    // Celebratory debug message! 
     nice5.printSchema()
     println("it's working!")
+
+
 
     val words =  nice5.select($"by", $"time", $"id", split($"clean_sentences", " ").as("words"))
     val words_alone = words.drop("clean_sentences")
@@ -49,22 +60,17 @@ object HN_intake {
   	.setMinDF(2)
   	.fit(words_alone)
 
-  	cvModel.transform(words_alone).show(true)
-
-  	words_alone.printSchema()
-  	words_alone.collect()
-  	words_alone.printSchema()
+  	val hashable = cvModel.transform(words_alone).show(false)
 
     val mh = new MinHashLSH().setNumHashTables(5).setInputCol("features").setOutputCol("sent_hash")
 
-    val with_hash = mh.fit(words_alone)
-    words_alone.printSchema()
+    val hashed = mh.fit(words_alone)
+    
 
     println("\n\n\n\n\n\n\n\n\n\nhere!")
-    with_hash.transform(words_alone).show()
+    val finale = hashed.transform(words_alone).show(false)
     println("\n\n\n\n\n\n\n\n\n\nhere!")
 
-    words_alone.printSchema()
 
     // sentences.write.format("jdbc").option("url", url).option("dbtable", "sentences").option("user", "postgres").mode("overwrite").option("driver", "org.postgresql.Driver").option("password", "L0ngfins").save()
     words_alone.write.format("jdbc").option("url", url).option("dbtable", "yay!").option("user", "postgres").mode("overwrite").option("driver", "org.postgresql.Driver").option("password", "L0ngfins").save()
